e9bcaab342a5051596af7af014161e4a90cce49b
amirhf <amirhacking85@gmail.com>
2023-06-21 20:40:23 +0330

    register DM on app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\DocumentService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->singleton(DocumentService::class, function ($app) {
+            return new DocumentService();
+        });
     }
 }
